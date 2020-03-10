1300d482ac2c0ff26c35237e47d65f108fbe650b
Tomas Pesek <tomas.pesek@shopsys.com>
2020-03-10 23:50:36 +0100

    updated homepage

--- src/components/Homepage/Homepage.vue
+++ src/components/Homepage/Homepage.vue
@@ -1,18 +1,23 @@
 <template>
 	<div>
 		<h1>Homepage</h1>
-		<ul>
-			<li class="nav-item"><router-link to="/order" class="nav-link">Order</router-link></li>
-			<li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
-		</ul>
-
-		Cart items: {{ cartBox.items }}
-		Cart totalPrice: {{ cartBox.totalPrice }}
+		
+		<router-link to="/order" class="nav-link" v-if="cartBox.items > 0">
+			Cart items: {{ cartBox.items }}
+			Cart totalPrice: {{ cartBox.totalPrice }}
+		</router-link>
+		<div v-else>
+			Cart is empty
+		</div>
 
 		<h3>Special offers</h3>
 		<div class="list-products">
 			<ProductItem v-for="product in products" :key="product.uuid" :product=product />
 		</div>
+
+		<ul>
+			<li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
+		</ul>
 	</div>
 </template>
 
